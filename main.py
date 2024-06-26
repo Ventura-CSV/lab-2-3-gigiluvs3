aeda8e086d6ec62e19667110d641193df9895cc4
Giovanna Ruiz <g3327388@gmail.com>
2024-06-26 14:14:36 -0700

    ch 1 lad 2

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    val1 = int(input('input a value '))
+    val2 = int(input('input a value '))
+    val3 = int(input('input a value '))
+    total = val1 + val2 + val3 
+    average = total/3
+    print ('Values:', val1, val2, val3)
+    print (f'Total: \t\t {total}')
+    print (f'Average: \t {average:.2f}')
     """
     Use following variables to save your result
     total 
@@ -16,3 +24,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
